850d4809916d3dcac87fb2aa9dad24593b83225b
jeffersonmatias <138624962+jeffersonmatias@users.noreply.github.com>
2024-01-11 18:12:24 -0300

    adding responsive

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { flatMap } from 'rxjs';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css', './menu-bar.responsive.component.css' ]
+  styleUrls: ['./menu-bar.component.css', './menu-bar.responsive.component.css']
 })
 export class MenuBarComponent implements OnInit {
+
+  checkboxValue: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openMenu() {
+    this.checkboxValue = true
+  }
+
+  closeMenu() {
+    this.checkboxValue = false
+  }
 }
